2ac6b2b81755eafe78f4d3ac347a06181549a0bc
marcuskelly09 <marcuskelly2909@gmail.com>
2025-07-15 16:26:59 -0400

    init

---
+++ desktopinstall.sh
@@ -0,0 +1,6 @@
+
+sudo pacman -S linux-zen-headers nvidia-dkms nvidia-settings nvidia-utils egl-wayland uwsm sddm waybar hyprland xdg-desktop-portal-hyprland hyprpaper hyprpolkitagent hyprshot pipewire-pulse pavucontrol firefox
+
+# INSERT CODE TO ADD lib32 Nvidia Utils Configuration (needs to be appended to pacman.conf?)
+
+sudo pacman -S lib32-nvidia-utils
