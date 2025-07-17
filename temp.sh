2faa465fb6082541db0553a7dea08081d41c8470
Marcus Kelly <121195649+marcuskelly09@users.noreply.github.com>
2025-07-17 01:18:04 -0400

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -24,6 +24,7 @@ arch-chroot /mnt <<EOF
     locale-gen
     echo "LANG=en_US.UTF-8" > /etc/locale.conf
     echo "laptoparch" > /etc/hostname
+    refind-install
 EOF
 
 echo "run umount -R /mnt when done"
