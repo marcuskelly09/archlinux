e4e795d97ed284ce8415b01b50ce402243fd14c0
Marcus Kelly <121195649+marcuskelly09@users.noreply.github.com>
2025-07-16 23:50:51 -0400

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -18,7 +18,7 @@ mkdir /mnt/{boot,home,var}
 mount /dev/nvme0n1p1 /mnt/boot
 mount -o subvol=@home /dev/nvme0n1p2 /mnt/home
 mount -o subvol=@var /dev/nvme0n1p2 /mnt/var
-pacstrap -K /mmt base linux-zen linux-firmware sof-firmware amd-ucode networkmanager nano vim man-db man-pages reflector sudo
+pacstrap -K /mnt base linux-zen linux-firmware sof-firmware amd-ucode networkmanager nano vim man-db man-pages reflector sudo
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
