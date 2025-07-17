7928c55c61a58f0589dce8b2a5ba16de56752fc4
Marcus Kelly <121195649+marcuskelly09@users.noreply.github.com>
2025-07-16 23:28:40 -0400

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -13,7 +13,7 @@ mount /dev/nvme0n1p1 /mnt/boot
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@var
-pacstap -K /mmt base linux-zen linux-firmware sof-firmware amd-ucode networkmanager nano vim man-db man-pages reflector sudo
+pacstrap -K /mmt base linux-zen linux-firmware sof-firmware amd-ucode networkmanager nano vim man-db man-pages reflector sudo
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
