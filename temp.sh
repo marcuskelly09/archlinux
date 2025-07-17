0534e748f5c7edc357048cc1d5f7b9707710332d
Marcus Kelly <121195649+marcuskelly09@users.noreply.github.com>
2025-07-16 23:03:56 -0400

    Update temp.sh
    Creates /boot directory and mounts EFI partition to it

--- temp.sh
+++ temp.sh
@@ -1,4 +1,5 @@
 # This is a note of commands i run to setup arch (barebones for G14)
+umount -a
 wipefs -a /dev/nvme0n1
 parted -s /dev/nvme0n1 mklabel gpt
 parted -s /dev/nvme0n1 mkpart primary fat32 1Mib 3Gib
@@ -7,6 +8,8 @@ parted -s /dev/nvme0n1 mkpart primary btrfs 3Gib 100%
 mkfs.fat /F32 /dev/nvme0n1p1
 mkfs.btrfs -f /dev/nvme0n1p2
 mount /dev/nvme0n1p2 /mnt
+mkdir /mnt/boot
+mount /dev/nvme0n1p1 /mnt/boot
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@var
