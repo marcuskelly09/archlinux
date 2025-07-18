fb07348a1c6332ed84da2e21a2411793721f9adf
marcuskelly09 <marcuskelly2909+github@gmail.com>
2025-07-18 01:06:12 -0400

    Added partitioning + subvolume creation

--- archinstall.sh
+++ archinstall.sh
@@ -21,38 +21,115 @@ function check_drive_validity () {
 	fi
 }
 
+function custom_drive_selection {
+	if [[ $input == "y" || $input == "" ]]; then
+		drive="/dev/nvme0n1"
+	else
+		# Custom drive selection
+		clear
+		get_drives
+		
+		# Lists all drives in format [x] /dev/sda
+		count=0
+		for temp_drive in "${all_drives[@]}"; do
+			((++count))
+			echo "[${count}] ${temp_drive}"
+		done
+
+		# User selection sreen
+		echo "Please select your drive:"
+		read input
+
+		# Checks if selected drive is valid
+		check_drive_validity $input
+
+		if [[ $func_output == "valid" ]]; then
+			# Take user input -1 as the index for all_drives
+			drive=${all_drives[$(($input - 1))]}
+		else
+			echo "ERROR: drive is invalid, aborting"
+			exit
+		fi
+	fi
+}
+
+function partition_drive {
+	wipefs -a $drive
+	parted -s $drive mklabel gpt
+	parted -s $drive mkpart primary fat32 1Mib 3Gib
+	parted -s $drive mkpart primary btrfs 3Gib 100%
+	parted -s ${drive}p1 set 1 esp on
+
+	mkfs.fat -F32 ${drive}p1
+	mkfs.btrfs -f ${drive}p2
+
+	mount ${drive}p2 /mnt
+
+	btrfs subvolume create /mnt/@
+	btrfs subvolume create /mnt/@home
+	btrfs subvolume create /mnt/@var
+
+	umount /mnt
+
+	mount -o subvol=@ ${drive}p2 /mnt
+
+	mkdir /mnt/{boot,home,var}
+	mkdir /mnt/boot/efi
+
+	mount ${drive}p1 /mnt/boot/efi
+
+	mount -o subvol=@home ${drive}p2 /mnt/home
+	mount -o subvol=@var ${drive}p2 /mnt/var
+
+	echo "Volumes successfully mounted"
+}
+
 # Script Starts
 clear
 echo "Welcome to Asus Arch Install"
 echo ""
-echo "Would you like to install Arch onto the default drive: /dev/nvme0n1? (y/n)"
+echo "On any (y/n) sequence, ENTER will default to y"
+echo ""
+
+echo "Would you to install with default options? (y/n)"
 read input
 
-# Custom options Selection
-if [[ $input == "y" || $input == "" ]]; then
+# Default installation prompt
+if [[ $input == "Y" || $input == "y" || $input == "" ]]; then
+	options="default"
+	echo "DEBUG: using default options"
+else
+	options="custom"
+fi
+
+clear
+echo "Custom Installation"
+echo
+# Drive selection prompt
+if [[ $options == "default" ]]; then
 	drive="/dev/nvme0n1"
 else
-	# Custom drive selection
-	clear
-	get_drives
-
-	count=0
-	for temp_drive in "${all_drives[@]}"; do
-		((++count))
-		echo "[${count}] ${temp_drive}"
-	done
-	echo "Please select your drive:"
+	echo "Would you like to install Arch onto the default drive: /dev/nvme0n1? (y/n)"
 	read input
 
-	# Checks if drive is valid
-	check_drive_validity $input
-
-	if [[ $func_output == "valid" ]]; then
-		drive=${all_drives[$(($input - 1))]}
+	# Custom installation selection
+	if [[ $input == "Y" || $input == "y" || $input == "" ]]; then
+		drive="/dev/nvme0n1"
 	else
-		echo "ERROR: drive is invalid, aborting"
-		exit
+		custom_drive_selection
 	fi
 fi
 
-echo "Using drive: ${drive}"
+
+clear
+echo "IMPORTANT: This script will WIPE your drive: ${drive} to install arch"
+echo ""
+echo "Press ENTER to continue, to abort press ctrl+c"
+read input
+
+
+exit
+### Destructive
+
+partition_drive
+
